29115d9ef4be1a1160d46bd7fba797da7f2d13d9
Seu Nome <seu.email@exemplo.com>
2026-03-24 13:51:49 -0300

    feat: Configure webpack to disable `fs` and `encoding` fallbacks for client-side builds.

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,16 @@ const nextConfig = {
             headers: [{ key: 'Content-Type', value: 'application/manifest+json' }],
         },
     ],
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                fs: false,
+                encoding: false,
+            };
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
